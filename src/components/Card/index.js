878ee79bf5fb8b3c2c0e1efbcd7c4e24aba8301b
svetanguyen <matematicaforeber@gmail.com>
2021-02-12 22:43:34 +0700

    add check, delete and input

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,29 +1,42 @@
 import React, { Component } from 'react';
 import cl from 'classnames';
-import s from './Card.module.css';
+import s from './Card.module.scss';
+import { CheckSquareOutlined, DeleteOutlined } from '@ant-design/icons';
 
 class Card extends React.Component {
 
     state = {
         chosen: false,
+        isRemembered: false, 
     }
 
-    handleCardClick = () => {
-            if (this.state.chosen === false) {
-                this.setState({
-                    chosen: true,
-                })
-            } else {
-                this.setState({
-                    chosen: false, 
-                })
-            }
+    handleCardClick = () => {  
+        if (this.state.isRemembered === false ) {
+            this.setState(({ chosen }) => {
+                return {
+                    chosen: !chosen,
+                }
+            })
+        }                    
             
         };
+    
+    handleIsRememberedClick = () => {
+        this.setState(({isRemembered}) => {
+            return {
+                isRemembered: !isRemembered,
+            }
+        });      
+    };
+
+    handleDeletedClick = () => {
+        console.log('###: 1 level');
+        this.props.onDeleted();
+    };
 
     render() {
         const {eng, rus} = this.props;
-        const {chosen} = this.state;
+        const {chosen, isRemembered} = this.state;
         const cardClass = [s.card];
         if (chosen) {
             cardClass.push(s.chosen);
@@ -32,16 +45,25 @@ class Card extends React.Component {
         return(
 
             <div 
-                className={ cl(s.card, { [s.chosen]: chosen}) }
-                onClick={this.handleCardClick}
+                className={ cl(s.card, { [s.chosen]: chosen},
+                    { [s.isRemembered]: isRemembered},
+                    { [s.chosen]: isRemembered}) }                
                 >
-                <div className={s.cardInner}>
+                <div className={s.icons}>
+                    <CheckSquareOutlined onClick={this.handleIsRememberedClick}/>    
+                </div>
+                
+                <div className={s.cardInner} onClick={this.handleCardClick}>
                     <div className={s.cardFront}>
                         { eng }
                     </div>
                     <div className={s.cardBack}>
                         { rus }
                     </div>
+                </div>
+
+                <div className={s.icons}>
+                    <DeleteOutlined onClick={this.handleDeletedClick} />
                 </div>
                 
             </div>
--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import s from './CardList.module.css';
 import Card from '../Card';
 
-const CardList = ({item = []}) => {
-	return(
-		<div className={s.root}>
-			{
+class CardList extends Component {
+	state = {
+		value: '',
+		label: '',
+	};
 
-          item.map(({eng, rus}, index) => 
-            <Card key={index} eng={eng} rus={rus}/>)
-  
-			}
-		</div>
-	)
+	inputRef = React.createRef();
+
+	handleInputChange = (e) => {
+			console.log('###: ', e.target.value);
+			this.setState({
+				value: e.target.value
+			})
+		};
+	handleSubmitForm = (e) => {
+		e.preventDefault();
+		 this.setState(({value}) => {
+		 	return {
+		 		label: value,
+		 		value: '', 
+		 	}
+		 });
+	};
+	render() {
+		const {item = [], onDeletedItem} = this.props;
+
+		return (
+		<>
+			<div>
+				{this.state.label}
+			</div>
+			<form className={s.form} onSubmit={this.handleSubmitForm}>
+				<input ref={this.inputRef} type="text" value={this.state.value} 
+					onChange={this.handleInputChange} />
+				<button>
+					Add new word
+				</button>
+			</form>
+			<div className={s.root}>
+				{
+
+	          item.map(({eng, rus, id}) => 
+	            <Card onDeleted={() => {
+	            	console.log('###: 2 level');
+	            	onDeletedItem(id);
+	        	}} key={id} eng={eng} rus={rus}/>)
+	  
+				}
+			</div>
+		</>
+	);
+	}
 }
+
 
 export default CardList;
